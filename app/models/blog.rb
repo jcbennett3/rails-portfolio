2092d6113c6a82761f464e4099ae6d9352213a08
jcbennett3 <bennettjc1978@gmail.com>
2018-03-18 15:53:59 -0500

    Updated status for enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
+  enum post_status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318201737) do
+ActiveRecord::Schema.define(version: 20180318204400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180318201737) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "post_status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
