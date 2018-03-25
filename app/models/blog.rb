943693aa6790c9bbc3e0143291df54d66765084d
jcbennett3 <bennettjc1978@gmail.com>
2018-03-24 20:10:39 -0500

    implemented for blogs, skills and portfolio items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,7 @@ class Blog < ApplicationRecord
   enum post_status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 300 }
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,8 @@
-class Portfolio < ApplicationRecord 
-    mount_uploader :thumb_image, PortfolioUploader 
-    mount_uploader :main_image, PortfolioUploader
+class Portfolio < ApplicationRecord
+  validates_presence_of :title, :body, :subtitle
+  validates :title, length: { maximum: 60 }
+  validates :subtitle, lenght: { maximum: 60 }
+  validates :body, length: { maximum: 500 }
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 end
